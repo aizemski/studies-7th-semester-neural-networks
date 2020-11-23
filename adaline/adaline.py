51f3122eb669f15031472e2370cfa3157bdb624e
aizemski <aleksanderizemski@gmail.com>
2020-11-23 11:24:16 +0100

    More readable adaline

--- adaline/adaline.py
+++ adaline/adaline.py
@@ -2,67 +2,69 @@ import numpy as np
 import matplotlib.pyplot as plt
  
 class Adaline(object):
- 
   def __init__(self, no_of_inputs, learning_rate=0.01, iterations=1000, biased = False):
     self.no_of_inputs = no_of_inputs
     self.learning_rate = learning_rate
     self.iterations = iterations
     self.biased = biased
+    
     if self.biased:
-      self.weights = np.random.random(2*self.no_of_inputs+1) -0.5 #Bias
+      self.weights = np.random.random(2*self.no_of_inputs+1) -0.5 #with bias
     else:
       self.weights = np.random.random(2*self.no_of_inputs)-0.5
-  
+
     self.errors = []
- 
-  def train(self, training_data_x, training_data_y):
-    # training_data_x = self._standarize(training_data_x)git
-    training_data_x = self._normalize(training_data_x) #normalizacja danych
-    training_data_y = self._normalize(training_data_y)
-    training_data_x = [i * 0.8 + 0.1 for i in training_data_x]
-    training_data_y = training_data_y * 0.8 + 0.1
+
+  def train(self, train_data, labels):
+    
+    train_data = self.normalize(train_data)
+    labels = self.normalize(labels)
+
+    train_data = train_data*0.8+0.1    
+    labels = labels * 0.8 + 0.1
+    
     for _ in range(self.iterations):
       e = 0
-      data = list(zip(training_data_x, training_data_y))
+      data = list(zip(train_data, labels))
       np.random.shuffle(data)
-      #for x,y in zip(training_data_x, training_data_y): 
-      for x, y in data: #losowe probki
+      for x, y in data: 
         x = np.concatenate([x, fourier_transform(x)])
         out = self.output(x)
+        
+        #update weights
         if self.biased:
-          self.weights[1:] += self.learning_rate * (y-out) * x * out * (1-out) #aktywacja - zamiast mnozenia *1 mamy tu (out)(1-out)
+          self.weights[1:] += self.learning_rate * (y-out) * x * out * (1-out)
           self.weights[0] += self.learning_rate * (y-out) * out * (1-out)
         else:    
-          self.weights += self.learning_rate * (y-out) * x #* out #* (1-out) 
+          self.weights += self.learning_rate * (y-out) * x 
+        
         e += (y - out)**2
       self.errors.append(e)
-    plt.plot(range(len(self.errors)), self.errors)
-    plt.savefig('learning_curve.pdf')
-    plt.close()
- 
-  def _standarize(self, training_data_x):
-    X = (training_data_x - np.mean(training_data_x)) / np.std(training_data_x)
-    return X
- 
-  def _normalize(self, training_data_x):
-    X = (training_data_x - np.min(training_data_x)) / (np.max(training_data_x)-np.min(training_data_x))
-    return X
- 
-  def _activation(self, x): 
-    # return x
-    return 1/(1 + np.exp((-1) * x))
- 
+      
+    #saving plot of error
+    for i in range(len(labels)):
+      if labels[i]:
+        plt.plot(range(len(self.errors)), self.errors)
+        plt.savefig('learning_curve_{}.pdf'.format(i))
+        plt.close()
+
+  def normalize(self, train_data):
+      return(train_data - np.min(train_data)) / (np.max(train_data)-np.min(train_data))
+
+  def activation(self, x): 
+      return 1/(1 + np.exp((-1) * x))
+
   def output(self, input):
-    if self.biased:
-      summation = self._activation(np.dot(self.weights[1:], input) + self.weights[0]) #Bias
-    else:
-      summation = self._activation(np.dot(self.weights, input))
-    return summation
+      if self.biased:
+          return self.activation(np.dot(self.weights[1:], input) + self.weights[0]) #Bias
+      return self.activation(np.dot(self.weights, input))
+         
+
   def predict (self,x):
-    x= self._normalize(x)
-    if self.biased:
-      return self._activation( np.dot(self.weights[1:],np.concatenate([x,fourier_transform(x)]))+self.weights[0])
-    return self._activation( np.dot(self.weights[1:],np.concatenate([x,fourier_transform(x)])))
+      x= self.normalize(x)
+      if self.biased:
+          return self.activation( np.dot(self.weights[1:],np.concatenate([x,fourier_transform(x)]))+self.weights[0])
+      return self.activation( np.dot(self.weights[1:],np.concatenate([x,fourier_transform(x)])))
 
 def fourier_transform(x):
   a = np.abs(np.fft.fft(x))
